b1297af62d039538db19d95579a4502580bb08eb
aniversariperu <aniversariperu1@gmail.com>
2014-05-13 00:10:26 +0300

    get tweets

--- bot.py
+++ bot.py
@@ -4,7 +4,12 @@
 import argparse
 from argparse import RawTextHelpFormatter
 import codecs
+import json
 import sys
+
+import requests
+
+import api
 
 
 description = u'Monitorea tuiter en búsqueda de publidad estatal'
@@ -30,9 +35,42 @@ def get_user_list():
     return user_list
 
 
+def get_recent_tweets(user_list):
+    oauth = api.get_oauth()
+
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    for user in user_list:
+        data = False
+        twitter_handle = user[1].replace("@", "")
+        payload = {
+            'screen_name': twitter_handle,
+            'count': 2,
+            }
+        try:
+            r = requests.get(url, auth=oauth, params=payload)
+            data = r.json()
+            for i in data:
+                tweet = {}
+                tweet['tweet_id'] = i['id']
+                tweet['screen_name'] = i['user']['screen_name']
+                tweet['user_id'] = i['user']['id']
+                tweet['status'] = i['text']
+                f = open("tuits.csv", "w")
+                f.write(json.dumps(tweet, indent=4))
+                f.close()
+                sys.exit(0)
+        except requests.exceptions.ConnectionError:
+            print("Error", r.text)
+
+
 def main():
+    # publicidad estatal prohibida desde el 24 de Enero
+
+    # authorities list
     user_list = get_user_list()
-    print user_list
+
+    get_recent_tweets(user_list)
+
 
 
 if __name__ == "__main__":
